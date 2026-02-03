f6b7d0f458283eecca67d6c62da52009526dd488
SamerBerjawi <117451075+SamerBerjawi@users.noreply.github.com>
2026-02-03 12:12:50 +0100

    Delete genAiLoader.ts

--- genAiLoader.ts
+++
@@ -1,9 +0,0 @@
-import { GoogleGenAI } from "@google/genai";
-
-export type GenAiChat = any;
-export type GenAiClient = any;
-export type GenAiModule = any;
-
-export const loadGenAiModule = async (): Promise<any> => {
-  return { GoogleGenAI };
-};
